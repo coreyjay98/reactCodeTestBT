58d4d4715f9401a43d07dcf91f89bca52a522012
Corey Samuels <coreyjsamuels@gmail.com>
2021-03-03 14:21:38 +0000

    Initialising files and NPM
    Npm init command and adding intitial boiler plate files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const myData = {
+  source: {
+    id: null,
+    name: "redmondpie.com",
+  },
+  author: "Oliver Haslam",
+  title:
+    "Tesla’s Full Self-Driving Subscription Will Apparently Land Within 4 Months",
+  description:
+    "Tesla's Full Self-Driving subscription will apparently land within 4 months. Here is everything you need to know about this. [ Continue reading this over at RedmondPie.com ]",
+  url:
+    "https://www.redmondpie.com/teslas-full-self-driving-subscription-will-apparently-land-within-4-months/",
+  urlToImage:
+    "https://cdn.redmondpie.com/wp-content/uploads/2018/03/Tesla-main-1200.jpg",
+  publishedAt: "2021-03-02T19:02:45Z",
+  content:
+    "Tesla founder and CEO Elon Musk says that the company will launch its subscription service for its Full Self-Driving package within the next three or four months after what feels like more delays tha… [+1304 chars]",
+};
