11de548569704ff9f67b9564c23c0af9fc5d35bd
Corey Samuels <coreyjsamuels@gmail.com>
2021-03-03 20:20:04 +0000

    Header/Footer styling and main content
    Adding styles to Header and Footer
    
    Adding main content component which is wherearticles will be searched and displayed
    
    added search input form and button

--- js/MainContent.js
+++ js/MainContent.js
@@ -1,3 +1,12 @@
 const MainContent = () => {
-  return <div className="mainContent">Main Content</div>;
+  return (
+    <div className="mainContent">
+      <h1 className="title">Search For An Article</h1>
+      <div className="searchInputForm">
+        <input name="searchInput" type="text"></input>
+        <button>Search</button>
+      </div>
+      <div className="articleDisplay"></div>
+    </div>
+  );
 };
--- js/MainLayout.js
+++ js/MainLayout.js
@@ -2,10 +2,12 @@
 const MainLayout = () => {
   return (
     <div className="App">
+      {/* Header  */}
       <div className="navBar ">
         <h1 className="title">BT React Code Test</h1>
       </div>
       <MainContent />
+      {/* Footer */}
       <div className="footer">
         <h4>Corey Samuels 3/4/2021</h4>
       </div>
